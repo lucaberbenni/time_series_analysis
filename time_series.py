de9f7e6dfa423c663daec6581f80bfd77439c172
Luca Berbenni <lucaberbenni@mbp-di-luca.localdomain>
2023-05-24 10:46:03 +0200

    code cleaned

--- time_series.py
+++ time_series.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
